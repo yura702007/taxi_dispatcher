a4daf5f5cef21db66d18bf8adad9780fa7786c38
yura702007 <82802568+yura702007@users.noreply.github.com>
2021-07-21 22:32:44 +0300

    add message_handler function

--- bot_taxi/handlers.py
+++ bot_taxi/handlers.py
@@ -5,3 +5,9 @@ from config import ADMIN_ID
 
 async def send_to_admin(dp):
     await bot.send_message(chat_id=ADMIN_ID, text='Бот запущен')
+
+@dp.message_handler()
+async def echo(message: Message):
+    text = f'Привет, ты написал {message.text}'
+    # await bot.send_message(chat_id=message.from_user.id, text=text)
+    await message.answer(text=text)
